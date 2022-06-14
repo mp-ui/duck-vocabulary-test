dc37c699cbeedcfad00d00aeaee35b6eda7ef031
Prince <1012096056@qq.com>
2022-06-14 14:50:39 +0800

    项目初始化

---
+++ duck-vocabulary-test/src/main/java/com/liuduck/DuckVocabularyTestApplication.java
@@ -0,0 +1,17 @@
+package com.liuduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Prince
+ * @since 2022-06-14
+ */
+@SpringBootApplication
+public class DuckVocabularyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckVocabularyTestApplication.class, args);
+    }
+
+}
---
+++ duck-vocabulary-test/src/test/java/com/liuduck/DuckVocabularyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckVocabularyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
