f0430c8d60bcc5b5edfdf2faf44fd182b7c8a3b2
dzh <8797974+dzh25288@user.noreply.gitee.com>
2022-06-14 17:01:06 +0800

    JsonConvertUtil.java  json文件转java对象存数据库

--- duck-vocabulary-test/src/main/java/com/liuduck/util/JsonConvertUtil.java
+++ duck-vocabulary-test/src/main/java/com/liuduck/util/JsonConvertUtil.java
@@ -1,5 +1,18 @@
 package com.liuduck.util;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.liuduck.entity.Word;
+import com.liuduck.mapper.WordMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author DZHooo
  * @version 1.0
@@ -7,5 +20,60 @@ package com.liuduck.util;
  * @description: TODO
  */
 
+@Component
 public class JsonConvertUtil {
+
+    @Resource
+    private WordMapper wordMapper;
+
+    public static void func() {
+
+        List<Word> wordList = new ArrayList<>();
+
+//        List<String> jsonList = getDataFromJsonFile("duck-vocabulary-test/CET6_1.json");
+        List<String> jsonList = getDataFromJsonFile("duck-vocabulary-test/CET4_3.json");
+
+        for (String json : jsonList) {
+            JSONObject jsonObject = JSON.parseObject(json);
+            String word = (String) jsonObject.get("headWord");
+            String mean = (String) jsonObject.getJSONObject("content").
+                    getJSONObject("word").
+                    getJSONObject("content").
+                    getJSONArray("trans").getJSONObject(0).get("tranCn");
+            wordList.add(new Word(word, 1, mean));
+        }
+
+        for (Word w : wordList) {
+            System.out.println(w);
+        }
+
+    }
+
+
+    public static List<String> getDataFromJsonFile(String jsonFilePath) {
+        List<String> jsonList = new ArrayList<>();
+        File file = new File(jsonFilePath);
+        if (file.exists()) {
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+                String s = null;
+                while ((s = bufferedReader.readLine()) != null) {
+                    jsonList.add(s);
+                }
+            } catch (IOException e) {
+                System.out.println("=====获取数据异常=====" + e);
+            } finally {
+                if (null != bufferedReader) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException ex) {
+                        System.out.println("=======获取数据时，关闭流异常=======" + ex);
+                    }
+                }
+            }
+        }
+        return jsonList;
+    }
+
 }
--- duck-vocabulary-test/src/test/java/com/liuduck/DuckVocabularyTestApplicationTests.java
+++ duck-vocabulary-test/src/test/java/com/liuduck/DuckVocabularyTestApplicationTests.java
@@ -1,13 +1,20 @@
 package com.liuduck;
 
+import com.liuduck.util.JsonConvertUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 
 @SpringBootTest
 class DuckVocabularyTestApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void func() {
+        JsonConvertUtil.func();
     }
 
 }
