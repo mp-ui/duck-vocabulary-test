ee7834bfb32593244844a5c98e08e82a49ebf8a5
lzk <1801290586@qq.com>
2022-06-14 15:56:14 +0800

    添加跨域请求

---
+++ duck-vocabulary-test/src/main/java/com/liuduck/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.liuduck.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author lzk
+ * @Email 1801290586@qq.com
+ * @Description <类说明>跨域问题
+ * @Date 15:38 2022/6/14
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
